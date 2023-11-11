feeb0baa4ad0e8aaa330b48821c34c77a26b342f
danielFeliseed <daniel@feliseed.co.jp>
2023-11-11 12:35:09 +0900

    Template created

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+  export let data;
+</script>
+
+<ul>
+  {#each data.countries as country}
+    <li >{country.name}</li>
+  {/each}
+</ul>
+
+<button class="bg-black rounded color-red" >Hey</button>
